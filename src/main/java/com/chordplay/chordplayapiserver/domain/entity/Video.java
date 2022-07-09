aee24bacb7ab539243e465fd4eb9d6c435ce15db
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-09 22:11:17 +0900

    Modify VIDEO db field

--- src/main/java/com/chordplay/chordplayapiserver/domain/entity/Video.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/entity/Video.java
@@ -14,10 +14,9 @@ public class Video {
     private String id;
 
     @Builder
-    public Video(String id, String thumnail_path, String vid_youtube, String title, String genre, String singer, int difficulty_avg, int play_count) {
+    public Video(String id, String thumnail_path, String title, String genre, String singer, int difficulty_avg, int play_count) {
         this.id = id;
         this.thumnail_path = thumnail_path;
-        this.vid_youtube = vid_youtube;
         this.title = title;
         this.genre = genre;
         this.singer = singer;
@@ -26,11 +25,14 @@ public class Video {
     }
 
     private String thumnail_path;
-    private String vid_youtube;
     private String title;
     private String genre;
     private String singer;
     private int difficulty_avg;
     private int play_count;
+
+    public Video(String id){
+        this.id = id;
+    }
 
 }
