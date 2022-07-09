ce7e91a7e5a14f5dc81b7666e0fae43d6def252f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-09 22:16:35 +0900

    feat: Add craeteAi in controller

--- src/main/java/com/chordplay/chordplayapiserver/api/sheet/SheetApiController.java
+++ src/main/java/com/chordplay/chordplayapiserver/api/sheet/SheetApiController.java
@@ -1,19 +1,27 @@
 package com.chordplay.chordplayapiserver.api.sheet;
 
+import com.chordplay.chordplayapiserver.dto.sheet.SheetAiRequest;
+import com.chordplay.chordplayapiserver.service.SheetGenerateService;
 import com.chordplay.chordplayapiserver.service.SheetService;
 import com.chordplay.chordplayapiserver.dto.sheet.SheetDataResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/sheet")
+@RestController
+@RequestMapping("/sheets")
 @RequiredArgsConstructor
 public class SheetApiController {
+    private final SheetGenerateService sheetGenerateService;
     private final SheetService sheetService;
+
+    @PostMapping("/ai")
+    public ResponseEntity<Void> sheetAi(@RequestBody SheetAiRequest dto){
+        sheetGenerateService.createAi(dto);
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<SheetDataResponseDTO> sheet(@PathVariable("id") String id){
