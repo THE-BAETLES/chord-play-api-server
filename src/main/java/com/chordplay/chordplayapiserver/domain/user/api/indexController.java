7024609c33b19b3453f89151944d94488d013f5f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-11 20:11:19 +0900

    Add spring security and begin login feacher

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/api/indexController.java
@@ -0,0 +1,49 @@
+package com.chordplay.chordplayapiserver.domain.user.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/auth")
+public class indexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String loginProc(){
+        return "joinProc";
+    }
+
+
+
+}
