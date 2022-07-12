ce2199855508cff535504d6805d77dc8bf2584fc
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-12 17:07:57 +0900

    Add Join request dto

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/dto/JoinRequest.java
@@ -0,0 +1,25 @@
+package com.chordplay.chordplayapiserver.domain.user.dto;
+
+import com.chordplay.chordplayapiserver.domain.user.TestUser;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString @Getter @Setter
+public class JoinRequest {
+    private String username;
+    private String password;
+    private String email;
+
+    @Builder
+    public JoinRequest(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public TestUser toEntity(String role){
+        return TestUser.builder().username(username).email(email).password(password).role(role).build();
+    }
+}
