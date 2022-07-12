63f4a0b268df80416baaa6ab9c67d3090075a1c7
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-12 17:07:14 +0900

    Add test user schema and repo

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/TestUser.java
@@ -0,0 +1,32 @@
+package com.chordplay.chordplayapiserver.domain.user;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document(collection = "TEST_USER")
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+public class TestUser {
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    @Builder
+    public TestUser(String id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/TestUserRepository.java
@@ -0,0 +1,5 @@
+package com.chordplay.chordplayapiserver.domain.user;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TestUserRepository extends MongoRepository<TestUser, String> {
+}
