85274e99f2586263aa3573e85c13115192bb37e8
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-14 01:43:29 +0900

    Add admin role

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/service/UserServiceImpl.java
@@ -14,12 +14,14 @@ public class UserServiceImpl implements UserService{
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final TestUserRepository testUserRepository;
 
+
+
     @Override
     public void join(JoinRequest dto) {
         String rawPassword = dto.getPassword();
         String encPassword = bCryptPasswordEncoder.encode(rawPassword);
         dto.setPassword(encPassword);
-        TestUser testUser = dto.toEntity("ROLE_USER");
+        TestUser testUser = (dto.getUsername().equals("admin")) ? dto.toEntity("ROLE_ADMIN") : dto.toEntity("ROLE_USER");
         testUserRepository.save(testUser);
     }
 }
