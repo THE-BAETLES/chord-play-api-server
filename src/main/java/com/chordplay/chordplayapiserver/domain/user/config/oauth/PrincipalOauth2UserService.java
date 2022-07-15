8032fd15ebe39199a8eb8978a89627aab4becf19
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-16 02:41:08 +0900

    Add jwt dependency

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/oauth/PrincipalOauth2UserService.java
@@ -6,15 +6,11 @@ import com.chordplay.chordplayapiserver.domain.user.config.auth.PrincipalDetails
 import com.chordplay.chordplayapiserver.domain.user.config.oauth.provider.GoogleUserInfo;
 import com.chordplay.chordplayapiserver.domain.user.config.oauth.provider.OAuth2UserIfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 
 @Service
