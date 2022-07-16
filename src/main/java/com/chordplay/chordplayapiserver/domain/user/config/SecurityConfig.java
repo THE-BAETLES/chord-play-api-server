9dbddf9802de19237efa656a341bdac67d4cf5f3
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-16 13:46:46 +0900

    Add JWT security config

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/config/SecurityConfig.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/SecurityConfig.java
@@ -1,20 +1,24 @@
 package com.chordplay.chordplayapiserver.domain.user.config;
 
 import com.chordplay.chordplayapiserver.domain.user.config.oauth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private PrincipalOauth2UserService principalOauth2UserService;
+    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final CorsFilter corsFilter;
     @Bean
     public BCryptPasswordEncoder encoderPwd(){
         return new BCryptPasswordEncoder();
@@ -23,16 +27,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
-                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .formLogin()
-                .loginPage("/loginForm").loginProcessingUrl("/login").defaultSuccessUrl("/index")
-                .and()
-                .oauth2Login().loginPage("/loginForm").userInfoEndpoint().userService(principalOauth2UserService);
+                .addFilter(corsFilter)
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
 
 
     }
---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/filter/MyFilter1.java
@@ -0,0 +1,4 @@
+package com.chordplay.chordplayapiserver.domain.user.config.filter;
+
+public class MyFilter1 {
+}
