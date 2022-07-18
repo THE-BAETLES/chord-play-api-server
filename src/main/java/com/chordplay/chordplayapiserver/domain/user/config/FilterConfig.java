fb88bf9dd6db4e330f8ef8be875eddbb18d6ffee
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-18 13:42:16 +0900

    Add custom filter example

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.chordplay.chordplayapiserver.domain.user.config;
+
+import com.chordplay.chordplayapiserver.domain.user.config.filter.MyFilter1;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1(){
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+        bean.addUrlPatterns("/*"); // 모든 요청
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
