683d8d5222870a1099642786a7f7370cb0153613
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-18 18:04:51 +0900

    Delete example filter

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/config/FilterConfig.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/FilterConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-    @Bean
-    public FilterRegistrationBean<MyFilter1> filter1(){
-        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
-        bean.addUrlPatterns("/*"); // 모든 요청
-        bean.setOrder(0);
-        return bean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<MyFilter1> filter1(){
+//        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+//        bean.addUrlPatterns("/*"); // 모든 요청
+//        bean.setOrder(0);
+//        return bean;
+//    }
 
 }
