3a97308cb26e65d17e0246b18e0546ac5476ed96
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-20 02:41:07 +0900

    Modify user url

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/api/apiController.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/api/apiController.java
@@ -1,17 +1,18 @@
 package com.chordplay.chordplayapiserver.domain.user.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/user")
 public class apiController {
 
-    @PostMapping("token")
-    public String token(){
-        return "<h1>token</h1>";
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(){
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/user")
