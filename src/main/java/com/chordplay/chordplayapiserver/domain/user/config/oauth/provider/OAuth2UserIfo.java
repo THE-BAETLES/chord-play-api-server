3a439246cf860ce88341f42634cb0ae6bb70602b
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-20 01:03:11 +0900

    Delete Oauth code
    firebase authentication 을 사용하기 때문에 필요 없어짐

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/config/oauth/provider/OAuth2UserIfo.java
+++
@@ -1,8 +0,0 @@
-package com.chordplay.chordplayapiserver.domain.user.config.oauth.provider;
-
-public interface OAuth2UserIfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-}
