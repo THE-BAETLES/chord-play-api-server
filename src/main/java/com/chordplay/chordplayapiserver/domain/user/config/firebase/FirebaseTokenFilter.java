737921234acbaaa2884deb0e3bd34f0e50f40f23
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-23 21:01:55 +0900

    Modify excluded url

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/config/firebase/FirebaseTokenFilter.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/config/firebase/FirebaseTokenFilter.java
@@ -36,7 +36,7 @@ public class FirebaseTokenFilter extends OncePerRequestFilter{
     private static final List<String> EXCLUDE_URL =
             Collections.unmodifiableList(
                     Arrays.asList(
-                            "/user/join"
+                            "/user/test"
                     ));
 
     @Builder
