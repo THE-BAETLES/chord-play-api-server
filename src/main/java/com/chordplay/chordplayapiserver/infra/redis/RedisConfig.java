5c4dd3eb476b1e890c8ccfdd8f5e4c2e1cb09fcc
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-26 23:07:36 +0900

    Update redis config

--- src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisConfig.java
+++ src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String host;
@@ -17,5 +20,12 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 }
