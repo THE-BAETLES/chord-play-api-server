e121a45c9a0862f9831277f067abfa719ffe94a5
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-26 20:26:15 +0900

    Add function that send to fifo queue

---
+++ src/main/java/com/chordplay/chordplayapiserver/infra/messageQueue/sqs/SqsMessageSender.java
@@ -0,0 +1,30 @@
+package com.chordplay.chordplayapiserver.infra.messageQueue.sqs;
+
+import com.chordplay.chordplayapiserver.infra.messageQueue.MessageQueue;
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SqsMessageSender implements MessageQueue {
+
+    private final QueueMessagingTemplate queueMessagingTemplate;
+    private final AmazonSQSProperties amazonSQSProperties;
+
+    @Override
+    public void sendToFifoQueue(String messagePayload, String messageGroupID, String messageDedupID) {
+
+        Message msg = MessageBuilder.withPayload(messagePayload)
+                .setHeader("message-group-id", messageGroupID)
+                .setHeader("message-deduplication-id", messageDedupID)
+                .build();
+        queueMessagingTemplate.send(amazonSQSProperties.getName(), msg);
+        log.info("message sent to " + amazonSQSProperties.getName());
+    }
+
+}
