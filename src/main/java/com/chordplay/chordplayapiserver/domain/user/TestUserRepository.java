54314a657786c072ec2e3cea04abaaf78b556c93
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-07-27 21:50:23 +0900

    Delete my example classes

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/TestUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chordplay.chordplayapiserver.domain.user;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestUserRepository extends MongoRepository<TestUser, String> {
-    public TestUser findByUsername(String username);
-    public TestUser findByFirebaseUid(String FirebaseUid);
-}
