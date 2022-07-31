7e6f46cafd52cb1099abccb8c6ecbe4f19a221ab
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-01 05:06:53 +0900

    Change from using Jackson2JsonRedisSerializer to using StringRedisSerializer

--- src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisService.java
+++ src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisService.java
@@ -1,6 +1,8 @@
     package com.chordplay.chordplayapiserver.infra.redis;
 
 
+    import com.fasterxml.jackson.core.JsonProcessingException;
+    import com.fasterxml.jackson.databind.ObjectMapper;
     import lombok.RequiredArgsConstructor;
     import org.springframework.data.redis.core.RedisTemplate;
     import org.springframework.data.redis.listener.ChannelTopic;
@@ -9,10 +11,12 @@
     @Service
     @RequiredArgsConstructor
     public class RedisService {
-        private final RedisTemplate<String, Object> redisTemplate;
+        private final RedisTemplate<String, String> redisTemplate;
 
-        public void publish(ChannelTopic topic, RedisMessage message){
-            redisTemplate.convertAndSend(topic.getTopic(), message);
+        public void publish(ChannelTopic topic, RedisMessage message) throws JsonProcessingException {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String value = objectMapper.writeValueAsString(message);
+            redisTemplate.convertAndSend(topic.getTopic(), value);
         }
 
 
