b95633278c09a383ddfe9dfb6771c4559d072537
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-04 04:35:01 +0900

    Add empty constructor for @RequestBody error

--- src/main/java/com/chordplay/chordplayapiserver/domain/sheet/dto/AiStatusMessage.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/sheet/dto/AiStatusMessage.java
@@ -1,8 +1,13 @@
 package com.chordplay.chordplayapiserver.domain.sheet.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AiStatusMessage {
     private String videoId;
     private int status;
@@ -10,5 +15,7 @@ public class AiStatusMessage {
     public AiStatusMessage(String videoId, int status) {
         this.videoId = videoId;
         this.status = status;
+    }
+    public AiStatusMessage(){
     }
 }
