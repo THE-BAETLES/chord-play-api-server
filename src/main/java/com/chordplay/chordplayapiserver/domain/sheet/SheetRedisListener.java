8716684aaf4e47d0fc0cf976d7c22162a106a8ab
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-04 19:13:27 +0900

    move subscriber class to sheet dir

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/sheet/SheetRedisListener.java
@@ -0,0 +1,34 @@
+package com.chordplay.chordplayapiserver.domain.sheet;
+
+import com.chordplay.chordplayapiserver.domain.sheet.dto.AiStatusMessage;
+import com.chordplay.chordplayapiserver.global.sse.CustomSseEmitter;
+import com.chordplay.chordplayapiserver.global.sse.service.NotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+import java.io.IOException;
+
+public class SheetRedisListener implements MessageListener {
+    private final ObjectMapper objectMapper;
+    private final NotificationService notificationService;
+    private CustomSseEmitter emitter;
+
+    @Builder
+    public SheetRedisListener(ObjectMapper objectMapper, NotificationService notificationService, CustomSseEmitter emitter) {
+        this.objectMapper = objectMapper;
+        this.notificationService = notificationService;
+        this.emitter = emitter;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            AiStatusMessage aiStatusMessage = objectMapper.readValue(message.getBody(), AiStatusMessage.class);
+            notificationService.sendStatusToClient(aiStatusMessage,emitter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
