4ac722aa9e6965609f828e71a09f43f0e4f1afcc
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-06 23:04:24 +0900

    Add function that find one sheet data

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetDataRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetDataRepository.java
@@ -1,7 +1,11 @@
 package com.chordplay.chordplayapiserver.domain.dao;
 
+import com.chordplay.chordplayapiserver.domain.entity.Sheet;
 import com.chordplay.chordplayapiserver.domain.entity.SheetData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface SheetDataRepository extends MongoRepository<SheetData,String> {
+    Optional<SheetData> findOneByVideoId(String videoId);
 }
