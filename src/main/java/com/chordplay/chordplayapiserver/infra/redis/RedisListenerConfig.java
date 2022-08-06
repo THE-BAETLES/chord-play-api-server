cdd3fe92b91e98e2a90d3bca680b4965fa001def
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-06 23:06:32 +0900

    Modify redis subscriber and delete bean annotation
    redis Listener 객채에 sse emitter을 포함시켜 요청 당 listener 객체를 생성하여 컨테이너에 추가시키기로 함 그러기위해선 싱글톤 해제를 위해 Bean annotation을 삭제함

--- src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisListenerConfig.java
+++ src/main/java/com/chordplay/chordplayapiserver/infra/redis/RedisListenerConfig.java
@@ -1,30 +1,22 @@
 package com.chordplay.chordplayapiserver.infra.redis;
 
+import com.chordplay.chordplayapiserver.domain.sheet.SheetRedisListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisListenerConfig {
 
+    private RedisMessageListenerContainer container;
     private final RedisConnectionFactory redisConnectionFactory;
-    private final RedisSubscriber redisSubscriber;
     @Bean
     RedisMessageListenerContainer redisContainer() {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        this.container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
-        container.addMessageListener(messageListenerAdapter(), new ChannelTopic("ch01"));
         return container;
-    }
-
-    @Bean
-    MessageListenerAdapter messageListenerAdapter() {
-        return new MessageListenerAdapter(redisSubscriber);
     }
 }
