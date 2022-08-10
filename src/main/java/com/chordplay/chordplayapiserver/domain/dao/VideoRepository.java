308c1bd19d3e99999368af7376cf6a9ca4a46551
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-10 22:20:07 +0900

    Add func that get sheet count

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/VideoRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/VideoRepository.java
@@ -1,7 +1,16 @@
 package com.chordplay.chordplayapiserver.domain.dao;
 
 import com.chordplay.chordplayapiserver.domain.entity.Video;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface VideoRepository extends MongoRepository<Video,String> {
+
+    @Aggregation(pipeline = {
+            "{ '$match': {'_id': ?0} }",
+            "{ '$lookup': { 'from': 'SHEET',  'localField': '_id',  'foreignField': 'video.$id', 'as': 'sheets' } }",
+            "{'$project': { '_id': 0, 'sheet_count': { '$size': '$sheets' } } }"
+    })
+    Long getSheetCount(String videoId);
+
 }
