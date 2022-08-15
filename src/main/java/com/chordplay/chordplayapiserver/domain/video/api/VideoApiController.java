5719060cac7ebbf63e392e2f8f68219ede114e03
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-15 15:48:35 +0900

    Add function that create video

--- src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
@@ -7,27 +7,29 @@ import com.chordplay.chordplayapiserver.domain.video.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/video")
+@RequestMapping("/videos")
 @RequiredArgsConstructor
 public class VideoApiController {
 
     private final VideoService videoService;
 
     @GetMapping(value = "/search/{search_query}")
-    public ResponseEntity<List<VideoResponse>>  search(@PathVariable("search_query") String search_query){
+    public ResponseEntity<List<VideoResponse>> search(@PathVariable("search_query") String search_query){
         System.out.println(search_query);
         List<VideoResponse> videoResponses = videoService.search(search_query);
         return ResponseEntity.ok(videoResponses);
     }
 
+    @PostMapping("/{videoId}")
+    public ResponseEntity<Void> create(@PathVariable("videoId") String videoId){
+        videoService.create(videoId);
+        return ResponseEntity.ok().build();
+    }
 
 }
