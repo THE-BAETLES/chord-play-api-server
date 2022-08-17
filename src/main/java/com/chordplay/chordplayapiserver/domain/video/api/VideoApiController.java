583ba64225547eb54230267541b74ab88a7f6d38
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-17 17:24:28 +0900

    Modify search api

--- src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
@@ -5,26 +5,25 @@ import com.chordplay.chordplayapiserver.domain.sheet.dto.SheetDataResponse;
 import com.chordplay.chordplayapiserver.domain.video.dto.VideoResponse;
 import com.chordplay.chordplayapiserver.domain.video.service.VideoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/video")
+@RequestMapping("/videos")
 @RequiredArgsConstructor
+@Slf4j
 public class VideoApiController {
 
     private final VideoService videoService;
 
-    @GetMapping(value = "/search/{search_query}")
-    public ResponseEntity<List<VideoResponse>>  search(@PathVariable("search_query") String search_query){
-        System.out.println(search_query);
+    @GetMapping(value = "/search")
+    public ResponseEntity<List<VideoResponse>> search(@RequestParam String search_query){
+        log.info("search: "+ search_query);
         List<VideoResponse> videoResponses = videoService.search(search_query);
         return ResponseEntity.ok(videoResponses);
     }
