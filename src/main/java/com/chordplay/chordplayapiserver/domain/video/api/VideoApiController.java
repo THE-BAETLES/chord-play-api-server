8ec16d9e1c2ecc3bf69b12f8d432095f69453189
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-17 18:45:30 +0900

    Add common api response

--- src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/video/api/VideoApiController.java
@@ -1,15 +1,11 @@
 package com.chordplay.chordplayapiserver.domain.video.api;
 
-import com.chordplay.chordplayapiserver.domain.sheet.dto.SheetAiRequest;
-import com.chordplay.chordplayapiserver.domain.sheet.dto.SheetDataResponse;
 import com.chordplay.chordplayapiserver.domain.video.dto.VideoResponse;
 import com.chordplay.chordplayapiserver.domain.video.service.VideoService;
+import com.chordplay.chordplayapiserver.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.ObjectId;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -22,10 +18,10 @@ public class VideoApiController {
     private final VideoService videoService;
 
     @GetMapping(value = "/search")
-    public ResponseEntity<List<VideoResponse>> search(@RequestParam String search_query){
+    public ApiResponse<List<VideoResponse>> search(@RequestParam String search_query){
         log.info("search: "+ search_query);
         List<VideoResponse> videoResponses = videoService.search(search_query);
-        return ResponseEntity.ok(videoResponses);
+        return ApiResponse.success(videoResponses, 200);
     }
 
 
