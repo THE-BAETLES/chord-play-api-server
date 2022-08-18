2c57044cf08f096039e219a48ba0e67c4bbd1356
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-19 02:04:19 +0900

    Add webClient (spring webflux) to request recommendation server

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/recommendation/RecommendationConfig.java
@@ -0,0 +1,15 @@
+package com.chordplay.chordplayapiserver.domain.recommendation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class RecommendationConfig {
+
+    @Value("${recommendation.url}")
+    String url;
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create(url);
+    }
+}
