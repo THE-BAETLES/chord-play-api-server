7be7ce425b5d203e033a881da64ebc312060353f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-21 02:12:44 +0900

    Add function that push signup-favorite

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/UserRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/UserRepository.java
@@ -1,14 +1,18 @@
 package com.chordplay.chordplayapiserver.domain.dao;
 
 import com.chordplay.chordplayapiserver.domain.entity.User;
+import com.chordplay.chordplayapiserver.domain.entity.Video;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Update;
 
 import java.util.List;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String>{
     public User findByUsername(String username);
     public User findByFirebaseUid(String FirebaseUid);
     public Boolean existsByNickname(String nickname);
     public User deleteByFirebaseUid(String FirebaseUid);
+    @Update("{ '$push' : { 'signup_favorite' : ?1 }}")
+    void findAndPushSignupFavoriteById(String id, List<String> signupFavorite);
 
 }
