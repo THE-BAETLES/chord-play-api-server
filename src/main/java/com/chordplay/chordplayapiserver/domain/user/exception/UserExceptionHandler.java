45b927bc5de39505ced1077d62affdf8bdde465d
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-22 17:18:55 +0900

    fix user error response types and add Error codes

--- src/main/java/com/chordplay/chordplayapiserver/domain/user/exception/UserExceptionHandler.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/user/exception/UserExceptionHandler.java
@@ -1,23 +1,29 @@
 package com.chordplay.chordplayapiserver.domain.user.exception;
 
+import com.chordplay.chordplayapiserver.global.exception.ErrorCode;
+import com.chordplay.chordplayapiserver.global.exception.ErrorResponse;
 import com.chordplay.chordplayapiserver.global.exception.dto.ExceptionResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class UserExceptionHandler {
 
     @ExceptionHandler(value = {NicknameDuplicationException.class})
-    public ResponseEntity<Object> handleNicknameDuplicationException(NicknameDuplicationException ex){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(ex);
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
+    public ResponseEntity<Object> handleNicknameDuplicationException(NicknameDuplicationException e){
+        log.error("Nickname Duplication Exception",e);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.NICKNAME_DUPLICATION);
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(value = {NotFullyJoinedException.class})
-    public ResponseEntity<Object> handleNotFullyJoinedException(NotFullyJoinedException ex){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(ex);
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> handleNotFullyJoinedException(NotFullyJoinedException e){
+        log.error("Nickname Duplication Exception",e);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.NOT_FULLY_JOINED);
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
--- src/main/java/com/chordplay/chordplayapiserver/global/exception/ErrorCode.java
+++ src/main/java/com/chordplay/chordplayapiserver/global/exception/ErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     // Common
+    NICKNAME_DUPLICATION(409,"U001","Nickname Duplication Exception"),
+    NOT_FULLY_JOINED(401,"U002", "You did not sign up for details, Please continue to signup"),
     SHEET_CREATION_FAIL(500, "S001", "Sheet creation failed : ai server failed"),
     SHEET_DATA_NOT_FOUND(400, "S002", "Sheet data not found"),
     SHEET_NOT_FOUND(400, "S003", "Sheet not found"),
@@ -16,6 +18,9 @@ public enum ErrorCode {
     IO_EXCEPTION_ERROR(500, "C001", "IoException: server error"),
     UNAUTHORIZED(401, "C401", "Unauthorized"),
     ;
+
+
+
 
     private final String code;
     private final String message;
