0413417ebb0da334fda4ec3a7cb7c92a9a1ebac6
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-22 15:59:15 +0900

    Fix Recommendation config

--- src/main/java/com/chordplay/chordplayapiserver/domain/recommendation/RecommendationConfig.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/recommendation/RecommendationConfig.java
@@ -2,14 +2,17 @@ package com.chordplay.chordplayapiserver.domain.recommendation;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class RecommendationConfig {
 
     @Value("${recommendation.url}")
     String url;
     @Bean
     public WebClient webClient() {
+
         return WebClient.create(url);
     }
 }
