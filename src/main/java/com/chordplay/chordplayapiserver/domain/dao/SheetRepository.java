867565d024c8da523534c4b9113bd137ab1813d9
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-08-24 02:24:54 +0900

    fix findOneByVideoId -> findFirstByVideoId

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SheetRepository extends MongoRepository<Sheet, String> {
-    Optional<Sheet> findOneByVideoId(String videoId);
+    Optional<Sheet> findFirstByVideoId(String videoId);
 
     @Query(sort = "{ created_at : 1 }")
     List<Sheet> findAllByVideoId(String videoId);
