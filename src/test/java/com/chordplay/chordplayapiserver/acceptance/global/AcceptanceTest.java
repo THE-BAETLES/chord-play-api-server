6605a3f8aff7559501b17dc327a133ff1d06cdd1
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-09-03 20:40:21 +0900

    Update setup that create a firebase token before test

--- src/test/java/com/chordplay/chordplayapiserver/acceptance/global/AcceptanceTest.java
+++ src/test/java/com/chordplay/chordplayapiserver/acceptance/global/AcceptanceTest.java
@@ -1,7 +1,10 @@
 package com.chordplay.chordplayapiserver.acceptance.global;
 
+import com.chordplay.chordplayapiserver.global.util.FirebaseUtil;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -9,9 +12,24 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 public class AcceptanceTest {
     @LocalServerPort
     int port;
+    private static FirebaseUtil firebaseUtil;
+    private static String firebaseToken;
 
+    @Autowired
+    public void setFirebaseUtil(FirebaseUtil firebaseUtil) {
+        AcceptanceTest.firebaseUtil = firebaseUtil;
+    }
+
+    public AcceptanceTest() {
+    }
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+        RestAssured.basePath = "/v1";
+        if (firebaseToken == null) firebaseToken = firebaseUtil.getAdminTokenByUid();
+    }
+
+    public static String getFirebaseToken() {
+        return firebaseToken;
     }
 }
