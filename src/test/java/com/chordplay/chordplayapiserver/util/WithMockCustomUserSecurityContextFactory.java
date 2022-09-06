dc955a625090154cc4eba1d5960840b28bdecd6f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-09-07 01:54:00 +0900

    test: Add security setting for unit test
    controller layer 유닛 테스트를 하기위해 jwt 토큰 인증 관련 필터를 비활성화 하고 security context에 User 객체를 직접 삽입하는 custom annotation 생성함
    
    user Details service를 등록해 실제로 User객체를 가져오게되면
    DB와 연동이 되고 repository bean도 등록해야하므로 unit test에 적절치 않다고 판단함
    
    UserDetails를 mockBean으로 하는 방식도 고려했지만 위 방법에 비해 크게 이점이 있지 않아 그대로 진행함

---
+++ src/test/java/com/chordplay/chordplayapiserver/util/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,31 @@
+package com.chordplay.chordplayapiserver.util;
+
+import com.chordplay.chordplayapiserver.domain.entity.User;
+import com.chordplay.chordplayapiserver.domain.user.config.auth.PrincipalDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        User user = getAdminUser();
+        UserDetails userDetails = new PrincipalDetails(user);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return context;
+    }
+    private User getAdminUser(){
+        return User.builder()
+                .id("6313b2381f8fa3bb122eaa78")
+                .username("최현준")
+                .roles("ROLE_USER")
+                .build();
+    }
+}
