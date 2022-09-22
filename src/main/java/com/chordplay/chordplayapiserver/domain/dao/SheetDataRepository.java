ef95cb1ca9cb4e6d9eb9a96538de6af60aabf214
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-09-22 11:13:53 +0900

    feat: Add a repository method for chord change

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetDataRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetDataRepository.java
@@ -2,10 +2,17 @@ package com.chordplay.chordplayapiserver.domain.dao;
 
 import com.chordplay.chordplayapiserver.domain.entity.Sheet;
 import com.chordplay.chordplayapiserver.domain.entity.SheetData;
+import com.chordplay.chordplayapiserver.domain.sheet.dto.SheetChangeRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 
 import java.util.Optional;
 
 public interface SheetDataRepository extends MongoRepository<SheetData,String> {
     Optional<SheetData> findOneById(String id);
+
+    @Query("{ 'id' : ?0 , 'chordInfos.position' : ?1}")
+    @Update("{ '$set' : { 'chordInfos.$.chord' : ?2 } }")
+    public void updateSheetChord(String sheetId, int position, String chord);
 }
