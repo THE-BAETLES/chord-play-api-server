e68432c3841640dade3b309e7ecb2b978bb6c18c
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-11 19:58:50 +0900

    Update Deployment shell

--- update_api.sh
+++ update_api.sh
@@ -1,3 +1,3 @@
 ./gradlew bootJar
-docker build --no-cache --platform=linux/amd64 -t pove2019/chord-play-api:0.0.4 .
-docker push pove2019/chord-play-api:0.0.4
+docker build --no-cache --platform=linux/amd64 -t 199349264777.dkr.ecr.ap-northeast-2.amazonaws.com/chord-play-api:0.0.4 .
+docker push 199349264777.dkr.ecr.ap-northeast-2.amazonaws.com/chord-play-api:0.0.4
