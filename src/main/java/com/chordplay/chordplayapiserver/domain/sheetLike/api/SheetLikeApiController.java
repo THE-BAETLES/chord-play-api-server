a6bb845eaabcbe648e79a8cda5022bb48136995f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-17 12:05:10 +0900

    feat: Add method of controller that add 'like'

---
+++ src/main/java/com/chordplay/chordplayapiserver/domain/sheetLike/api/SheetLikeApiController.java
@@ -0,0 +1,27 @@
+package com.chordplay.chordplayapiserver.domain.sheetLike.api;
+
+import com.chordplay.chordplayapiserver.domain.sheetLike.service.SheetLikeService;
+import com.chordplay.chordplayapiserver.global.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sheet-like")
+@RequiredArgsConstructor
+@Slf4j
+public class SheetLikeApiController {
+
+    SheetLikeService sheetLikeService;
+
+    @PostMapping("/{sheetId}")
+    public ResponseEntity<ApiResponse<String>> addLike(@PathVariable("sheetId") String sheetId){
+        sheetLikeService.addLike(sheetId);
+        ApiResponse<String> body = ApiResponse.success("", HttpStatus.CREATED.value());
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+    }
+
+
+}
