d6a33a5502a50fd54436f18f361390f39c8d9dfd
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-18 19:20:58 +0900

    feat: Add method of repo that delete the sheet-like

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetLikeRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetLikeRepository.java
@@ -1,7 +1,13 @@
 package com.chordplay.chordplayapiserver.domain.dao;
 
+import com.chordplay.chordplayapiserver.domain.entity.Sheet;
 import com.chordplay.chordplayapiserver.domain.entity.SheetLike;
+import com.chordplay.chordplayapiserver.domain.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface SheetLikeRepository extends MongoRepository<SheetLike,String> {
+
+    void deleteBySheetAndUser(Sheet sheet, User user);
 }
