590aa5d9245947efbbcf99d1616f3519fd1ce77e
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-21 21:07:35 +0900

    feat: Add method that find all sheet by user and video id

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepository.java
@@ -1,4 +1,6 @@
 package com.chordplay.chordplayapiserver.domain.dao;
+import com.chordplay.chordplayapiserver.domain.entity.User;
+import com.chordplay.chordplayapiserver.domain.entity.Video;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.chordplay.chordplayapiserver.domain.entity.Sheet;
 import org.springframework.data.mongodb.repository.Query;
@@ -11,5 +13,9 @@ public interface SheetRepository extends MongoRepository<Sheet, String> {
 
     @Query(sort = "{ created_at : 1 }")
     List<Sheet> findAllByVideoId(String videoId);
+
+    @Query(sort = "{ created_at : 1 }")
+    List<Sheet> findAllByUserAndVideoId(User user, String videoId);
+
     Sheet save(Sheet sheet);
 }
