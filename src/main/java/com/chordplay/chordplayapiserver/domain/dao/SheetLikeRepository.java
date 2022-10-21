f8f7036f684dbf812ae4865386e7bfd987280fe7
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-21 21:14:17 +0900

    feat: Add method of repo that find sheetLike by sheet and user

--- src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetLikeRepository.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/dao/SheetLikeRepository.java
@@ -5,9 +5,11 @@ import com.chordplay.chordplayapiserver.domain.entity.SheetLike;
 import com.chordplay.chordplayapiserver.domain.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SheetLikeRepository extends MongoRepository<SheetLike,String> {
 
+    Optional<SheetLike> findBySheetAndUser(Sheet sheet, User user);
     Optional<SheetLike> deleteBySheetAndUser(Sheet sheet, User user);
 }
