6ea96cb46cfe3dcb625733e18d2f3b5dfb2a518b
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-23 03:16:05 +0900

    docs: Add docs that get my information

--- src/test/java/com/chordplay/chordplayapiserver/domain/user/docs/UserTestDocs.java
+++ src/test/java/com/chordplay/chordplayapiserver/domain/user/docs/UserTestDocs.java
@@ -1,12 +1,15 @@
 package com.chordplay.chordplayapiserver.domain.user.docs;
 
+import com.chordplay.chordplayapiserver.domain.user.dto.MyInformationResponse;
 import com.chordplay.chordplayapiserver.domain.video.docs.VideoTestDocs;
 import com.chordplay.chordplayapiserver.global.docs.CommonTestDocs;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultHandler;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.chordplay.chordplayapiserver.util.ApiDocumentUtils.getDocumentRequest;
 import static com.chordplay.chordplayapiserver.util.ApiDocumentUtils.getDocumentResponse;
@@ -24,8 +27,8 @@ public class UserTestDocs {
     private static String getNameOfDocsOfAddingVideoIdToMyCollection = "user/add_video_id_to_my_collection";
     private static String getNameOfDocsOfDeletingVideoIdFromMyCollection  = "user/delete_video_id_to_my_collection";
     private static String getNameOfDocsOfGettingMyCollection  = "user/get_my_collection";
+    private static String getNameOfDocsOfGettingMyInformation  = "user/get_my_information";
     private static String getNameOfDocsOfGettingMyCollectionVideoIdList  = "user/get_my_collection_video_id_list";
-
     public static ResultHandler documentOnLoginSuccess() {
         return document(getNameOfDocsOfLoginSuccess,
                 getDocumentRequest(),
@@ -34,6 +37,38 @@ public class UserTestDocs {
                         headerWithName("Authorization").description("Bearer {token}")
                 )
         );
+    }
+
+    public static ResultHandler documentOnMyInformation() {
+        return document(getNameOfDocsOfGettingMyInformation,
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                        headerWithName("Authorization").description("Bearer {token}")
+                ),
+                responseFields(CommonTestDocs.commonDocs()),
+                responseFields(beneathPath("data").withSubsectionId("data"), MyInformationResponseFields())
+        );
+    }
+
+    private static List<FieldDescriptor> MyInformationResponseFields() {
+        return Arrays.asList(
+                fieldWithPath("id").type(JsonFieldType.STRING).description("유저 아이디"),
+                fieldWithPath("nickname").type(JsonFieldType.STRING).description("유저 닉네임"),
+                fieldWithPath("username").type(JsonFieldType.STRING).description("유저 이름"),
+                fieldWithPath("firebase_uid").type(JsonFieldType.STRING).description("유저 firebase 아이디"),
+                fieldWithPath("roles").type(JsonFieldType.STRING).description("유저 권한"),
+                fieldWithPath("email").type(JsonFieldType.STRING).description("유저 email"),
+                fieldWithPath("country").type(JsonFieldType.STRING).description("유저 국가"),
+                fieldWithPath("language").type(JsonFieldType.STRING).description("유저 언어"),
+                fieldWithPath("gender").type(JsonFieldType.STRING).description("유저 성별"),
+                fieldWithPath("performer_grade").type(JsonFieldType.STRING).description("유저 기타 숙련도"),
+                fieldWithPath("membership").type(JsonFieldType.STRING).description("유저의 등급"),
+                fieldWithPath("signup_favorite").type(JsonFieldType.ARRAY).description("유저의 초기 선택곡 리스트"),
+                fieldWithPath("my_collection").type(JsonFieldType.ARRAY).description("유저의 내 곡 목록 ")
+
+        );
+
     }
 
     public static ResultHandler documentOnNicknameRecommendation() {
