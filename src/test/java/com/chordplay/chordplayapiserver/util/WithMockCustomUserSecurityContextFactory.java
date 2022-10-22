92d6bd36f3ef6f87746559dc79eb8b7346d8ccb0
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-23 03:14:53 +0900

    Modify mock user

--- src/test/java/com/chordplay/chordplayapiserver/util/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/chordplay/chordplayapiserver/util/WithMockCustomUserSecurityContextFactory.java
@@ -1,6 +1,7 @@
 package com.chordplay.chordplayapiserver.util;
 
 import com.chordplay.chordplayapiserver.domain.entity.User;
+import com.chordplay.chordplayapiserver.domain.entity.item.*;
 import com.chordplay.chordplayapiserver.domain.user.config.auth.PrincipalDetails;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -8,6 +9,9 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
 
@@ -21,7 +25,7 @@ public class WithMockCustomUserSecurityContextFactory implements WithSecurityCon
         context.setAuthentication(authentication);
         return context;
     }
-    private User getAdminUser(boolean hasNickname){
+    public static User getAdminUser(boolean hasNickname){
         if (hasNickname){
             return User.builder()
                     .id("6313b2381f8fa3bb122eaa78")
@@ -29,6 +33,14 @@ public class WithMockCustomUserSecurityContextFactory implements WithSecurityCon
                     .email("test@gmail.com")
                     .nickname("test")
                     .roles("ROLE_USER")
+                    .firebaseUid("firebase_uid")
+                    .performerGrade(PerformerGrade.EXPERT)
+                    .signupFavorite(Arrays.asList("videoId1", "videoId2"))
+                    .myCollection(Arrays.asList("videoId3", "videoId4"))
+                    .gender(Gender.MALE)
+                    .country(Country.KR)
+                    .language(Language.kr)
+                    .membership(Membership.NORMAL)
                     .build();
         }
 
