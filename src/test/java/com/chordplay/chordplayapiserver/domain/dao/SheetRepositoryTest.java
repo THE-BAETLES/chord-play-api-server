8a0d9221c6fc3ba71c3bbe21240f906008d49f35
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-23 21:42:28 +0900

    test: add repo test that get sheet-like count

--- src/test/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepositoryTest.java
+++ src/test/java/com/chordplay/chordplayapiserver/domain/dao/SheetRepositoryTest.java
@@ -2,13 +2,17 @@ package com.chordplay.chordplayapiserver.domain.dao;
 
 import com.chordplay.chordplayapiserver.acceptance.global.AcceptanceTest;
 import com.chordplay.chordplayapiserver.domain.entity.Sheet;
+import com.chordplay.chordplayapiserver.domain.entity.SheetLike;
 import com.chordplay.chordplayapiserver.domain.entity.User;
 import com.chordplay.chordplayapiserver.domain.entity.Video;
 import com.chordplay.chordplayapiserver.domain.sheet.dto.SheetsResponse;
+import com.chordplay.chordplayapiserver.domain.sheet.service.SheetService;
+import com.chordplay.chordplayapiserver.domain.sheetLike.service.SheetLikeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -18,6 +22,9 @@ class SheetRepositoryTest extends AcceptanceTest {
 
     @Autowired
     private SheetRepository sheetRepository;
+
+    @Autowired
+    private SheetLikeRepository sheetLikeRepository;
 
 
 
@@ -33,6 +40,8 @@ class SheetRepositoryTest extends AcceptanceTest {
                 .user(user)
                 .video(video).build();
         Sheet savedSheet = sheetRepository.save(sheet);
+        SheetLike sheetLike = new SheetLike(user,sheet, LocalDateTime.now());
+        sheetLikeRepository.save(sheetLike);
 
         //when
 
@@ -41,6 +50,29 @@ class SheetRepositoryTest extends AcceptanceTest {
         //then
         assertThat(sheets.size()).isEqualTo(1);
         assertThat(sheets.get(0).getId()).isEqualTo(savedSheet.getId());
+
+    }
+
+    @Test
+    @DisplayName("sheet-like count 가져오기_sheetLike 1개 저장_1개 반환 성공")
+    public void getSheetLikeCount() throws Exception {
+
+        //get
+        Video video = Video.builder().id(getTestVideoId()).build();
+        User user = new User(getTestUserId());
+
+        Sheet sheet = Sheet.builder()
+                .user(user)
+                .video(video).build();
+        Sheet savedSheet = sheetRepository.save(sheet);
+        SheetLike sheetLike = new SheetLike(user,sheet,LocalDateTime.now());
+        sheetLikeRepository.save(sheetLike);
+
+        //when
+        Long sheetLikeCount = sheetRepository.getSheetLikeCount(sheet.getId());
+
+        //then
+        assertThat(sheetLikeCount).isEqualTo(1);
 
     }
 }
