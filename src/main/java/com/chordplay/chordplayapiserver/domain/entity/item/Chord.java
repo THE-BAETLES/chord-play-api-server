504dc2d07c0f04d77910d4aab8f67503473ed6c2
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-25 01:09:45 +0900

    Modify chord class

--- src/main/java/com/chordplay/chordplayapiserver/domain/entity/item/Chord.java
+++ src/main/java/com/chordplay/chordplayapiserver/domain/entity/item/Chord.java
@@ -1,9 +1,14 @@
 package com.chordplay.chordplayapiserver.domain.entity.item;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
 
 @Getter
+@NoArgsConstructor
 public class Chord {
+
     private String root = "none";
     private String triad = "none";
     private String bass = "none";
